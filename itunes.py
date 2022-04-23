d08edfdb8af8ecdbe962d8526f37a823a4553eda
Katie Valus <katievalus@Katies-MBP-2.public.axo.com>
2022-04-23 10:45:24 -0400

    genres!

--- itunes.py
+++ itunes.py
@@ -16,33 +16,26 @@ from top100songs import get_links
 def make_request(song_lst): 
     matches = []
     songs = []
+    genres = []
     for item in song_lst:
         song = item[0]
         artist = item[1]
         year = item[2]
         r = requests.get("https://itunes.apple.com/search", params = {'term': song,
                                             'media': 'music'})
+        
         result = json.loads(r.text)
         for info in result['results']:
-            if info['artistName'] == artist and info['trackName'] == song and info['kind'] == 'song' and (year in info['releaseDate']):
+            if info['artistName'] == artist and info['trackName'] == song and info['kind'] == 'song':
                 if song in songs:
                     continue
                 else:
                     songs.append(song)
-                    print(info)
-    #                 album = info['collectionName']
-    #                 releasedate = info['releaseDate']
-    #                 country = info['country']
-    #                 genre = info['primaryGenreName']
-    #                 full_song_data = {}
-    #                 full_song_data['song'] = song
-    #                 full_song_data['album'] = album
-    #                 full_song_data['album'] = album
-    #                 full_song_data['releasedate'] = releasedate
-    #                 full_song_data['country'] = country
-    #                 full_song_data['genre'] = genre
-    #                 matches.append(full_song_data)
-    # return matches
+                    #print(info)
+                    genre = info['primaryGenreName']
+                    genres.append((song, genre))
+    return
+
 
 song_lst = get_links()
 print(make_request(song_lst))
